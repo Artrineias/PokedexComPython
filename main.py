d476f88b5752865a21c90e7be965539be3b68efb
Artrineias <erik21yan@gmail.com>
2022-02-21 17:43:30 -0300

    210220221743

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from flask import Flask,render_template
 
-site = Flask(__name__)
+def inicial():
 
-@site.route("/")
-def index():
-    return render_template("index.html")
-
-
-@site.route("/lista/")
-def lista():
-    return render_template("lista.html")
+    site = Flask(__name__)
+    
+    @site.route("/")
+    def index():
+        return render_template("index.html")
 
 
-def inicial():    
-    site.run(debug=True)
+    @site.route("/lista/")
+    def lista():
+        return render_template("lista.html")
+
+    return site
+
+
 
 
 if __name__=="__main__":
