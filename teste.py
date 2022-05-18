57c4dbe8e776e4752492d34e7189e74f91fddd49
Artrineias <erik21yan@gmail.com>
2022-05-18 16:08:19 -0300

    remoção de teste

--- teste.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://w3schools.com/python/demopage2.php'
-
-#use the 'cookies' parameter to send cookies to the server:
-x = requests.get(url, cookies = {"favcolor": "Red"})
-
-print(x.text)
